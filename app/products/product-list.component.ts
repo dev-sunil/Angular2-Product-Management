60d53b173fa57b23ca3196fb04b1d72942c43084
ashish9342 <ashish9342@gmail.com>
2016-09-11 01:26:58 +0530

    Implementation of Interface(X, not in javascript) and Encapulating Component Styles

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,18 +1,20 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
+import { IProduct } from './product';
 
 @Component({
 	selector: 'pm-products',
-	templateUrl: 'app/products/product-list.component.html'
+	templateUrl: 'app/products/product-list.component.html',
+    styleUrls : ["app/products/product-list.component.css"];
 })
 
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
 	pageTitle: string ="Product List";
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = "cart";
-	products: any[] =	[
-    {
+	products: IProduct[] =	[
+    { 
         "productId": 1,
         "productName": "Leaf Rake",
         "productCode": "GDN-0011",
@@ -65,6 +67,9 @@ export class ProductListComponent{
 ];
     toggleImage():void{
         this.showImage=!this.showImage;
+    }
+    ngOnInit():void{
+        console.log("In OnInt")
     } 
 
 }
